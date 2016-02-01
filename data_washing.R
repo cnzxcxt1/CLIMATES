685d1cb0b11c6ee177b8bf1e206909a4dfcd9e56
CHEN Xiangtuo <xiangtuo.chen@student.ecp.fr>
2016-02-01 19:30:16 +0100

    dly2csv
    try to change the .dly files to .csv format

--- data_washing.R
+++ data_washing.R
@@ -9,3 +9,27 @@ for (i in 1:nbr_element){
   write.csv(temp,paste(elements[i],"_",nrow(temp),".csv",sep="")); 
 }
 getwd()
+
+
+
+
+firsts=c(1,12,18,22,30,38,46,54,62,70,78,86,94,102,110,118,126,134,142,150,158,166,174,182,190,198,206,214,222,230,238,246,254,262)
+lasts=c(11,17,21,28,36,44,52,60,68,76,84,92,100,108,116,124,132,140,148,156,164,172,180,188,196,204,212,220,228,236,244,252,260,268)
+Stations=c("USW00023170","USW00014804","USW00013782")
+filename=paste("D://R_3_0//digiplante//climates//datas//",Stations[2],".dly",sep="")
+file<-file(filename,"r")
+line=readLines(file,n=1)
+table= as.data.frame(matrix(0,nrow=1,ncol = 34))
+for (i in 1:34) {
+  table[1,i]=substr(line, firsts[i], lasts[i])
+}
+while( length(line) != 0 ) {
+  line= readLines(file,n=1)
+  temp= as.data.frame(matrix(0,nrow=1,ncol = 34))
+  for (i in 1:34) {
+    temp[1,i]=substr(line, firsts[i], lasts[i])
+  }
+  table=rbind(table,temp)
+}
+close(file)
+
--- station_plot.R
+++ station_plot.R
@@ -88,3 +88,46 @@ for (i in years){
   dev.off()
 }
 
+
+
+library(ggmap)
+library(maptools)
+library(maps)
+#dir.create("D://R_3_0/digiplante/climates/TMIN")
+setwd("D://R_3_0/digiplante/climates/TCOMMON/")
+years=2000:2016
+for (i in years){
+  filename=paste("D://R_3_0//digiplante//climates/TCOMMON/TCOMMON_",i,".csv",sep="")
+  mapinfo=read.table(filename,sep = ",",header = T)
+  mapinfo.x=mapinfo$LONGITUDE;
+  mapinfo.y=mapinfo$LATITUDE;
+  png(paste("TCOMMON_station_",i,".png",sep=""))
+  map("world", fill=TRUE, col="white", bg="lightblue",ylim=c(-60, 90), mar=c(0,0,0,0))
+  points(mapinfo.x,mapinfo.y, col="red", pch=16);
+  title(paste("TCOMMON_station_",i,sep=""));
+  dev.off()
+}
+
+library(ggmap)
+library(maptools)
+library(maps)
+setwd("D://R_3_0/digiplante/climates")
+years=2000:2016
+for (i in years){
+  i=2016
+  filename=paste("D://R_3_0//digiplante//climates/TCOMMON/TCOMMON_",i,".csv",sep="")
+  mapinfo=read.table(filename,sep = ",",header = T)
+  mapinfo.x=mapinfo$LONGITUDE;
+  mapinfo.y=mapinfo$LATITUDE;
+  mp <- NULL
+  mapWorld <- borders("world", colour="gray50", fill="gray50") 
+  # create a layer of borders
+  mp <- ggplot() +   mapWorld
+  
+  
+  #Now Layer the cities on top
+  mp <- mp+ geom_point(aes(x=mapinfo.x, y=mapinfo.y) ,color="blue", size=3) 
+  png(paste("TCOMMON_station_",i,".png",sep=""))
+  mp
+  dev.off()
+}
